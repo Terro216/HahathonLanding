c8d27783db6accc9c924218778e4308b09789219
Nick <nikitamm59rus@gmail.com>
2022-03-07 18:43:38 +0300

    Setup docker build

--- Jakefile.js
+++ Jakefile.js
@@ -1,27 +1,12 @@
 let exec = require('child_process').execSync;
 let fs = require('fs');
 
-// desc('Builds back');
-// task('buildBack', function () {
-//   return new Promise((resolve, reject) => {
-//     exec(`cd ./HahathonLanding-Back && npm ci && npm run build`, (err, stdout, stderr) => {
-//       if (err) {
-//         console.error(stderr);
-//         reject(stderr);
-//       }
-//
-//       console.log(stdout);
-//       resolve(true);
-//     });
-//   });
-// });
-
 desc('Build project.');
 task('default', function () {
+  // Build back
   exec('cd ./HahathonLanding-Back && npm ci && cd ..');
 
-  // fs.mkdirSync('./HahathonLanding-Back/src/static');
-
+  // Build front
   exec('cd ./HahathonLanding-Front && npm ci && npm run build && cd ..');
   jake.cpR('./HahathonLanding-Front/dist', './HahathonLanding-Back/src');
   fs.renameSync('./HahathonLanding-Back/src/dist', './HahathonLanding-Back/src/static');
