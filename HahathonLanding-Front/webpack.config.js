357a3f6d57781f682541d9ba2050419a50e7878e
IvLaptev <ivlaptev13@ya.ru>
2022-03-14 08:05:02 +0300

    Moved scripts to body

--- HahathonLanding-Front/webpack.config.js
+++ HahathonLanding-Front/webpack.config.js
@@ -37,7 +37,8 @@ module.exports = {
   plugins: [
     new htmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/index.pug'
+      template: './src/index.pug',
+      inject: 'body'
     }),
     new miniCssExtractPlugin(),
   ]
